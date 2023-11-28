8d888f6ba911241f38c18f14c92786578485bf50
lenam2809 <49664798+lenam2809@users.noreply.github.com>
2023-11-28 21:22:53 +0700

    create Application Commands

---
+++ EcomShop.Application/Commands/Customers/Delete/DeleteCustomerCommand.cs
@@ -0,0 +1,49 @@
+﻿using EcomShop.Core.Repositories.Command;
+using EcomShop.Core.Repositories.Query;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomShop.Application.Commands.Customers.Delete
+{
+    public class DeleteCustomerCommand : IRequest<String>
+    {
+        public Int64 Id { get; private set; }
+
+        public DeleteCustomerCommand(Int64 Id)
+        {
+            this.Id = Id;
+        }
+    }
+
+    // Customer delete command handler with string response as output
+    public class DeleteCustomerCommmandHandler : IRequestHandler<DeleteCustomerCommand, String>
+    {
+        private readonly ICustomerCommandRepository _customerCommandRepository;
+        private readonly ICustomerQueryRepository _customerQueryRepository;
+        public DeleteCustomerCommmandHandler(ICustomerCommandRepository customerRepository, ICustomerQueryRepository customerQueryRepository)
+        {
+            _customerCommandRepository = customerRepository;
+            _customerQueryRepository = customerQueryRepository;
+        }
+
+        public async Task<string> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var customerEntity = await _customerQueryRepository.GetByIdAsync(request.Id);
+
+                await _customerCommandRepository.DeleteAsync(customerEntity);
+            }
+            catch (Exception exp)
+            {
+                throw (new ApplicationException(exp.Message));
+            }
+
+            return "Customer information has been deleted!";
+        }
+    }
+}
---
+++ EcomShop.Application/Commands/Customers/Update/EditCustomerCommand.cs
@@ -0,0 +1,58 @@
+﻿using EcomShop.Application.DTOs;
+using EcomShop.Core.Entities;
+using EcomShop.Core.Repositories.Command;
+using EcomShop.Core.Repositories.Query;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomShop.Application.Commands.Customers.Update
+{
+    public class EditCustomerCommand : IRequest<CustomerResponse>
+    {
+
+        public Int64 Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class EditCustomerCommandHandler : IRequestHandler<EditCustomerCommand, CustomerResponse>
+    {
+        private readonly ICustomerCommandRepository _customerCommandRepository;
+        private readonly ICustomerQueryRepository _customerQueryRepository;
+        public EditCustomerCommandHandler(ICustomerCommandRepository customerRepository, ICustomerQueryRepository customerQueryRepository)
+        {
+            _customerCommandRepository = customerRepository;
+            _customerQueryRepository = customerQueryRepository;
+        }
+        public async Task<CustomerResponse> Handle(EditCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customerEntity = CustomerMapper.Mapper.Map<Customer>(request);
+
+            if (customerEntity is null)
+            {
+                throw new ApplicationException("There is a problem in mapper");
+            }
+
+            try
+            {
+                await _customerCommandRepository.UpdateAsync(customerEntity);
+            }
+            catch (Exception exp)
+            {
+                throw new ApplicationException(exp.Message);
+            }
+
+            var modifiedCustomer = await _customerQueryRepository.GetByIdAsync(request.Id);
+            var customerResponse = CustomerMapper.Mapper.Map<CustomerResponse>(modifiedCustomer);
+
+            return customerResponse;
+        }
+    }
+}
